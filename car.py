ab59865a824a067992899951aaae888499d08b5e
akshat-61 <akshat74269@gmail.com>
2024-12-20 19:04:45 +0530

    Changes

--- car.py
+++ car.py
@@ -4,3 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+
+
+data = pd.read_csv('car data.csv')
+
+print(data)
